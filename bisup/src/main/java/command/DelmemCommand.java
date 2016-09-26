542d1e9d5fbac568f7ef52b9ecfab5914268b23b
user1 <user1@user1-PC>
2016-09-26 10:06:47 +0900

    command 내용 작성, 주석 추가

--- bisup/src/main/java/command/DelmemCommand.java
+++ bisup/src/main/java/command/DelmemCommand.java
@@ -1,38 +1,20 @@
 package command;
-
+//Å»ÅğÈ¸¿ø
 public class DelmemCommand {
 
-	private String var1;
-	private boolean var2;
-	private int var3;
-	
-	public DelmemCommand() {
-		// TODO Auto-generated constructor stub
+	private String id; //¾ÆÀÌµğ
+	private String del;//Å»ÅğÀÏ
+	public String getId() {
+		return id;
 	}
-	
-	public DelmemCommand(String var1, int var3) {
-		// TODO Auto-generated constructor stub
-		this.var1 = var1;
-		this.var3 = var3;
+	public void setId(String id) {
+		this.id = id;
 	}
-	
-	public String getVar1() {
-		return var1;
+	public String getDel() {
+		return del;
 	}
-	public void setVar1(String var1) {
-		this.var1 = var1;
-	}
-	public boolean isVar2() {
-		return var2;
-	}
-	public void setVar2(boolean var2) {
-		this.var2 = var2;
-	}
-	public int getVar3() {
-		return var3;
-	}
-	public void setVar3(int var3) {
-		this.var3 = var3;
+	public void setDel(String del) {
+		this.del = del;
 	}
 	
 }
