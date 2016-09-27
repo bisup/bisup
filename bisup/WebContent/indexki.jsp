101d6320e5b60dde1a3d642cd2fb06b2f1d9565c
back <jagjayo@gmail.com>
2016-09-27 12:36:58 +0900

    메뉴등록 열심히 작업중

--- bisup/WebContent/indexki.jsp
+++ bisup/WebContent/indexki.jsp
@@ -4,9 +4,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
-<a href="tiles/test/test.do">상단 테스트</a><br/>
-
-  
+<META http-equiv=refresh content="0;url=bisup_login/main.do"/>
 </body>
 </html>
--- bisup/src/main/java/controller/MainController.java
+++ bisup/src/main/java/controller/MainController.java
@@ -2,19 +2,26 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/main.do")
 public class MainController {
 
 	public MainController() {
 	// TODO Auto-generated constructor stub
 }
-	@RequestMapping("/main.do")
-	public String loginmain(){
-		System.out.println("main요청");
-		return "loginmain";//
-	}
 	
+	@RequestMapping(method = RequestMethod.GET)
+	public String main(){
+		System.out.println("main요청");
+		return "bisup_login/loginmain";//
+	}
+	@RequestMapping(method = RequestMethod.POST)
+	public String submit(){
+		System.out.println("로그인");
+		return "tiles/test/test";//
+	}
 	/*public ModelAndView modelandviewCreateView(){
 		ModelAndView modelandview = 
 	}*/
