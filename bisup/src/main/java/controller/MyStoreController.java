44dad91836eaa7085aba6e073a97d9dd0b60b22e
back <jagjayo@gmail.com>
2016-09-29 08:55:31 +0900

    컨트롤러 파일 설정 변경(통합)

--- bisup/src/main/java/controller/MyStoreController.java
+++ bisup/src/main/java/controller/MyStoreController.java
@@ -12,19 +12,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import command.MenuCommand;
 import dao.MyStoreAction;
+import dao.SaleProgressAction;
 import net.sf.json.JSONObject;
   
 @Controller
-public class MyStoreMenuController {
+public class MyStoreController {
 
 	private MyStoreAction myStoreAction;
 	
 	public void setMyStoreAction(MyStoreAction myStoreAction) {
 		this.myStoreAction = myStoreAction;
+	}
+	
+	private SaleProgressAction saleProgressAction;
+
+	public void setSaleProgressAction(SaleProgressAction saleProgressAction) {
+		this.saleProgressAction = saleProgressAction;
 	}
   
 	@RequestMapping("/menuMain.do")
@@ -66,5 +74,19 @@ public class MyStoreMenuController {
 		}
 		PrintWriter printWriter = response.getWriter();
 		printWriter.print(jsonObject.toString());
+	}
+	
+	@RequestMapping(value="/saleProgress.do", method=RequestMethod.POST)
+	@ResponseBody
+	public void drawDayChart(HttpServletResponse response, 
+			@RequestParam("id") String id) throws Exception{
+		System.out.println("drawDayChart메소드 도착, id는 "+id);
+		ArrayList fiveSales = saleProgressAction.salePerDay(id);
+		
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("fiveSales", fiveSales);
+		PrintWriter printWriter = response.getWriter();
+		printWriter.print(jsonObject.toString());
+		
 	}
 }
