d3bf54878c1289f2af8b1371968b3f835f989a52
user1 <user1@user1-PC>
2016-09-29 15:13:54 +0900

    0920 기철

--- bisup/src/main/java/controller/LoginController.java
+++ bisup/src/main/java/controller/LoginController.java
@@ -53,12 +53,16 @@ public class LoginController{
 		}
 		try{
 			int selLog = loginDAO.selLog(memberCommand.getId(), memberCommand.getPw());
-			if(selLog==1){
-				System.out.println("로그인");
-				return "bisup/main/main.do";
-			}else 
-			{
-				
+			if(selLog==0){
+				System.out.println("관리자 로그인");
+				return "bisup_login/main/main.do";
+			}else if(selLog == 1){
+				System.out.println("자영업자 로그인");
+				return "bisup_login/main/main.do";
+			}else if(selLog == 2){
+				System.out.println("창업예정자 로그인");
+				return "bisup_login/main/main.do";
+			}else{
 				System.out.println("꺼져");
 				return "bisup_login/loginfail";
 			}
