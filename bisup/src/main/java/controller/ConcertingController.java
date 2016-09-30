3e2f258df476177257ac672b3ea6b56b15d5dc53
coco <coco@DESKTOP-36TFSVM>
2016-09-30 09:34:33 +0900

    수정

--- bisup/src/main/java/controller/ConcertingController.java
+++ bisup/src/main/java/controller/ConcertingController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ConcertingController {
-
+	
+	/*//타일즈 적용
 	@RequestMapping("/bisup_concerting/info_customer.do")
 	public String info_customerView(){
 		System.out.println("info_customer요청");
@@ -25,7 +26,25 @@ public class ConcertingController {
 	public String relationView(){
 		System.out.println("relation요청");
 		return "relation";
+	}*/
+	
+	//개인 인덱스 적용
+	@RequestMapping("info_customer.do")
+	public String info_customerView(){
+		System.out.println("info_customer요청");
+		return "bisup_concerting/info_customer";
 	}
 	
+	@RequestMapping("manual.do")
+	public String manualView(){
+		System.out.println("manual요청");
+		return "bisup_concerting/manual";
+	}
+	
+	@RequestMapping("relation.do")
+	public String relationView(){
+		System.out.println("relation요청");
+		return "bisup_concerting/relation";
+	}
 	
 }
