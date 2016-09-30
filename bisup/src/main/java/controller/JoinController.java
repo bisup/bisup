0452bdaa20a5cacfbb6765f12b8d7ed1564c88ae
user1 <user1@user1-PC>
2016-09-30 10:12:14 +0900

    dd

--- bisup/src/main/java/controller/JoinController.java
+++ bisup/src/main/java/controller/JoinController.java
@@ -1,19 +1,26 @@
 package controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import command.MemberCommand;
 import dao.JoinDAO;
-import dao.LoginDAO;
+import service.MemberService;
 
 @Controller
 
 public class JoinController {
-	
+	private MemberService service;
 	@Autowired
 	private JoinDAO JoinDAO;
 	
@@ -41,10 +48,33 @@ public class JoinController {
 		return "joinForm";
 	}
 	
-	@RequestMapping(value="/join/form.do",method=RequestMethod.POST)
-	public String joinForm(@ModelAttribute("member") MemberCommand MemberCommand){
-		JoinDAO.insertMem();
-		
-		return "joinForm";
-	}
+	@RequestMapping("memberList.do")
+    public ModelAndView dummy2(){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("memberList",service.selectAll());
+        mav.setViewName("selectAll");
+        return mav;
+    }
+	
+	@RequestMapping("logout.do")
+    public String logout(HttpSession session){
+//        session.invalidate();
+        session.removeAttribute("id");
+        return "redirect:loginForm.do";
+    }
+	
+	 @RequestMapping("memberUpdateForm.do")
+	    public String memberUpdateForm(Model model,HttpSession session){
+	        String id = (String) session.getAttribute("id");
+	        if(id == null)
+	            return "redirect:loginForm.do";
+	        model.addAllAttributes(service.getMemberInfo(id));
+	        return "memberUpdateForm"; //¾÷µ¥ÀÌÆ® Æû
+	    }
+	 
+	 @RequestMapping("memberUpdate.do")
+	    public String memberUpdate(@RequestParam HashMap<String, Object> params){
+	        service.memberUpdate(params);
+	        return "redirect:main.do";
+	    }	
 }
