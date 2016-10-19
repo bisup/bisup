e2a7fe3441071776a27d02eef9ddba17302c2516
user1 <user1@user1-PC>
2016-10-19 12:17:01 +0900

    주석 정리

--- bisup/src/main/java/controller/AreaInfoController.java
+++ bisup/src/main/java/controller/AreaInfoController.java
@@ -41,21 +41,14 @@ public class AreaInfoController {
 	
 	@RequestMapping(value="/areaInfo/chartview.do",method=RequestMethod.GET)
 	public ModelAndView chartview(HttpServletRequest request, @RequestParam(value = "chartlist") String[] chartlist){
-		System.out.println("파이차트길이는="+chartlist.length);
+		//차트 가져오는 메서드
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("list",chartlist);
 		mav.setViewName("bisChart");
 		return mav; 
 	}
 	
-	/*@RequestMapping(value="/areaInfo/chartbar.do",method=RequestMethod.GET)
-	public ModelAndView chartBar(HttpServletRequest request, @RequestParam(value = "chartlist1") String[] chartlist1){
-		System.out.println("막대차트길이는="+chartlist1.length);
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("list1",chartlist1);
-		mav.setViewName("barChart");
-		return mav; 
-	}*/
+	
 	
 	@RequestMapping(value="/areaInfo/mpInfo.do",method=RequestMethod.POST)
 	public void sidoList(HttpServletResponse resp,@RequestParam("gcode") int gcode) throws Exception{
